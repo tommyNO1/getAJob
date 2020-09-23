5462fb8febf988141004814408df200052b0270e
tommyNO1 <905390519@qq.com>
2020-09-23 11:14:01 +0800

    增加了617题合并二叉树

---
+++ leetCode/src/com/leetcode/num617/MergeTwoBinaryTrees.java
@@ -0,0 +1,37 @@
+package com.leetcode.num617;
+
+public class MergeTwoBinaryTrees {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        return dfs(t1,t2);
+    }
+
+    private TreeNode dfs(TreeNode t1, TreeNode t2) {
+        if(t1==null&&t2==null){
+            return null;
+        }
+        if(t1!=null&&t2!=null){
+            t1.val +=t2.val;
+        }
+        if(t1==null&&t2!=null){
+            return t2;
+        }
+        if(t1!=null&&t2==null){
+            return t1;
+        }
+        TreeNode left = dfs(t1.left,t2.left);
+        TreeNode right = dfs(t1.right,t2.right);
+        t1.left=left;
+        t1.right=right;
+        return t1;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
